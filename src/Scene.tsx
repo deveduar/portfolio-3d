2130642494f4c122ddf631b5f4d9d1e2b75d04a0
Devedu <deveduar@gmail.com>
2024-05-10 01:31:55 +0200

    disable page scroll with event listener in Scene component

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,16 +1,30 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef } from "react";
 
 interface SceneProps {
   children: ReactNode;
 }
 
 function Scene({ children }: SceneProps) {
-  
+  const canvasRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = (event: WheelEvent) => {
+      if (canvasRef.current?.contains(event.target as Node)) {
+        event.preventDefault();
+      }
+    };
+
+    document.body.addEventListener("wheel", handleScroll, { passive: false });
+
+    return () => {
+      document.body.removeEventListener("wheel", handleScroll);
+    };
+  }, []);
 
   return (
-    <div className="canvas-container">
+    <div className="canvas-container" ref={canvasRef}>
       <Canvas>
         {children}
         <ambientLight intensity={0.5} />
